195d5526c56c341a74af8515284008effb809c7e
GeorgeBatalov <43064043+GeorgeBatalov@users.noreply.github.com>
2018-10-25 13:57:42 +0300

    Started at 15.10.2018

---
+++ BeerSong.java
@@ -0,0 +1,36 @@
+class BeerSong
+{
+
+public static void main (String [] args)
+{
+
+int beerNum = 5;
+String word = "bottles";
+
+while(beerNum>0)
+{
+	
+	if(beerNum==1){
+word = "BOTTLE";
+System.out.println(" LAST " + beerNum + word + " ON THE WALL ");
+
+}
+System.out.println(beerNum +" "+word+ " beer on the wall BITCH MF ");
+System.out.println(beerNum +" "+word+ " beer ");
+System.out.println("Take the one ");
+System.out.println(" give it others  ");
+beerNum--;
+
+
+}
+
+
+if(beerNum==0){
+System.out.println("YOU AR DRUNK ");
+
+}
+
+
+}
+
+}
---
+++ DooBee.java
@@ -0,0 +1,25 @@
+class DooBee //page 43 
+{
+public static void main (String[] args)
+
+
+{
+
+int x = 1;
+while (x<3){
+
+System.out.print("Doo");
+System.out.print("Bee");
+
+x++;
+}
+
+if(x==x) //link to itself? lol
+{
+System.out.print("Do");
+}
+
+}
+
+
+}
